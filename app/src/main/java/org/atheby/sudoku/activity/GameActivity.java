63a4dae3b8c084b40546189c9a3cf5b0f8b73fd8
atheby <atheby.the@gmail.com>
2017-04-05 18:20:14 +0200

    GameActivity added

---
+++ app/src/main/java/org/atheby/sudoku/activity/GameActivity.java
@@ -0,0 +1,14 @@
+package org.atheby.sudoku.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import org.atheby.sudoku.R;
+
+public class GameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+    }
+}
